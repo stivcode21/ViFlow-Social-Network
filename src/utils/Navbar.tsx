374464b8d5fe1c2d33f41ad7793d2ed087585fac
stivcode21 <stivenelcodificador@gmail.com>
2025-02-20 16:12:01 -0500

    small adjustments to the navigation bar

--- src/utils/Navbar.tsx
+++ src/utils/Navbar.tsx
@@ -10,17 +10,19 @@ import {
 import { Link } from "react-router-dom";
 
 const btnStyle =
-  "W-full h-full hover:bg-[#171717] text-[#888] hover:text-white px-6 py-2 lg:px-3 rounded-xl transition-all ease-in-out";
+  "W-full h-full hover:bg-[#171717] text-[#888] hover:text-white px-7 sm:px-9 md:px-3 py-2 rounded-xl transition-all ease-in-out";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="w-full lg:w-16 h-14 p-2 lg:h-full fixed blur-effect-theme text-white flex lg:flex-col justify-center lg:justify-between items-center  bottom-0 left-0">
+    <nav className="w-full md:w-16 h-14 p-2 md:h-full fixed blur-effect-theme text-white flex md:flex-col justify-center md:justify-between items-center  bottom-0 left-0">
+      {/* Logo */}
       <img
         src="/logo.png"
         alt="logo"
-        className="w-10 h-10 object-contain hidden lg:block mt-2"
+        className="w-10 h-10 object-contain hidden md:block mt-2"
       />
-      <div className="flex lg:flex-col gap-4">
+      {/* Iconos */}
+      <div className="w-full flex md:flex-col md:gap-4 justify-between items-center sm:mx-4 md:mx-0">
         <Link className={`${btnStyle}`} to={"/home"}>
           <HomeIcon className="w-8 h-8" />
         </Link>
@@ -37,8 +39,9 @@ const Navbar: React.FC = () => {
           <UserIcon className="w-8 h-8" />
         </Link>
       </div>
-      <div className="p-2 text-[#888] hover:text-white ">
-        <Bars3BottomLeftIcon className="w-8 h-8 hidden lg:block mb-2" />
+      {/* Menu icon */}
+      <div className="p-2 text-[#888] hover:text-white hidden md:block">
+        <Bars3BottomLeftIcon className="w-8 h-8 mb-2" />
       </div>
     </nav>
   );
